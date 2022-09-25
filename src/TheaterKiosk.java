6d15588cfd22284c56113842916fd025ab2095fd
Ji'Yahna <jiymeade@gmail.com>
2022-09-25 09:51:03 -0400

    Completed TheaterKiosk

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,11 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        System.out.println("How old are you?");
+        int personAge = 20;
+        if (personAge >= 21){
+            System.out.println("You get a Wrist Band");
+        }else if (personAge <= 20){
+            System.out.println(" No WristBand for you ");
+        }
+    }
+}
